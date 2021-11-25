4e703c84a610830e250c1f72c04b8a88931abf29
zhengxunWu1 <zw227@cornell.edu>
2021-11-25 12:35:18 -0500

    Remove ignored .env file

--- truffle-config.js
+++ truffle-config.js
@@ -22,7 +22,10 @@ const HDWalletProvider = require('@truffle/hdwallet-provider');
 require("dotenv").config();
 const fs = require('fs');
 const mnemonic = fs.readFileSync(".secret").toString().trim();
-console.log(process.env.ETHERSCAN_API_KEY)
+if (!process.env.ETHERSCAN_API_KEY) {
+  console.error("please set etherscan api key")
+}
+// console.log(process.env.ETHERSCAN_API_KEY)
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
